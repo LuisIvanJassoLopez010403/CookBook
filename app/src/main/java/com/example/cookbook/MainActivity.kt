e3263cb346d62b93c0ffd44b4b2e1fdebe8da25b
Luis Ivan Jasso Lopez <11995@ulsachihuahua.edu.mx>
2024-09-23 18:43:14 -0600

    Se agrega comentario

--- app/src/main/java/com/example/cookbook/MainActivity.kt
+++ app/src/main/java/com/example/cookbook/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cookbook.ui.theme.CookBookTheme
-
+//Clase principal
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
