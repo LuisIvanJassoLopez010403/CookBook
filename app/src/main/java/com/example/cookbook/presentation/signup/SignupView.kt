73cc507776cabfcb13681aa93c832d0fc72d8332
Mauricio Hernandez <mauriciohernandez@MacBook-Air-de-Mauricio.local>
2024-10-18 22:19:44 -0600

    Finalizacion de las pantallas Sign Up y My recipe View en base a las indicaciones del examen.

--- app/src/main/java/com/example/cookbook/presentation/signup/SignupView.kt
+++ app/src/main/java/com/example/cookbook/presentation/signup/SignupView.kt
@@ -1,14 +1,17 @@
 package com.example.cookbook.presentation.signup
 
-import androidx.compose.foundation.BorderStroke
-import com.example.cookbook.R
+import android.app.DatePickerDialog
+import android.widget.DatePicker
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,134 +22,168 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.cookbook.R
 import com.example.cookbook.navigation.Routes
+import java.util.*
 
 @Composable
 fun SignupView(navController: NavController) {
-    Row(
-        horizontalArrangement = Arrangement.Start
-    ) {
-        TextButton(onClick = { navController.navigate(Routes.TitleView) }) {
+    var email by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+    var confirmPassword by remember { mutableStateOf(TextFieldValue("")) }
+    var adress by remember { mutableStateOf(TextFieldValue("")) }
+    var birthDate by remember { mutableStateOf("") }
+
+    val calendar = Calendar.getInstance()
+    val datePickerDialog = DatePickerDialog(
+        navController.context,
+        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            birthDate = "$dayOfMonth/${month + 1}/$year"
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)
+    )
+
+    var expanded by remember { mutableStateOf(false) }
+    var selectedGender by remember { mutableStateOf("") }
+    val genderOptions = listOf("Male", "Female", "Other")
+
+    Box(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+        // Back Button
+        TextButton(
+            onClick = { navController.navigate(Routes.TitleView) },
+            modifier = Modifier.align(Alignment.TopStart) // Alinea el botón a la izquierda
+        ) {
             Text(
                 text = "< Back",
                 fontSize = 18.sp,
                 color = Color(0xFFFFA500),
-                modifier = Modifier,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Left
             )
         }
-    }
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(10.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
 
-
-        //Spacer(modifier = Modifier.height(70.dp))
-
-        //Image Logo
-        Image(
-            painter = painterResource(id = R.drawable.cookbooklogo3),
-            contentDescription = "Cookbook Logo",
-            modifier = Modifier
-                .size(250.dp)
-                .clip(CircleShape),
-            contentScale = ContentScale.Crop
-        )
-
-        //Spacer(modifier = Modifier.height(40.dp))
-
-// Title Text
-        Text(
-            text = "Sign up",
-            fontSize = 28.sp,
-            fontWeight = FontWeight.Bold
-        )
-
-
-        //Spacer(modifier = Modifier.height(16.dp))
-
-
-// Email Input
-        var email by remember { mutableStateOf(TextFieldValue("")) }
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text(text = "Email:") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-
-        //Spacer(modifier = Modifier.height(8.dp))
-
-
-// Username Input
-        var username by remember { mutableStateOf(TextFieldValue("")) }
-        OutlinedTextField(
-            value = username,
-            onValueChange = { username = it },
-            label = { Text(text = "Username:") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-
-        //Spacer(modifier = Modifier.height(8.dp))
-
-
-// Password Input
-        var password by remember { mutableStateOf(TextFieldValue("")) }
-        var passwordVisible by remember { mutableStateOf(false) }
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text(text = "Password:") },
-            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
-        )
-
-
-        //Spacer(modifier = Modifier.height(8.dp))
-
-
-// Username Input
-        var confirm by remember { mutableStateOf(TextFieldValue("")) }
-        OutlinedTextField(
-            value = confirm,
-            onValueChange = { confirm = it },
-            label = { Text(text = "Confirm Password:") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-
-        //Spacer(modifier = Modifier.height(32.dp))
-
-
-        // Sign up Button
-        Button(
-            onClick = { navController.navigate(Routes.TitleView) },    //Modificar la ruta a la pantalla inicial
-            colors = ButtonDefaults.buttonColors(Color.White),
+        // Column for the rest of the content
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(50.dp)
-                .border(1.5.dp, Color(0xFFFFA500), RoundedCornerShape(25.dp))
-                .shadow(10.dp, RoundedCornerShape(25.dp))
+                .padding(top = 40.dp), // Espacio para que el botón no se superponga con el contenido
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Sign Up", fontSize = 18.sp, color = Color(0xFFFFA500))
-        }
+            // Image Logo
+            Image(
+                painter = painterResource(id = R.drawable.cookbooklogo3),
+                contentDescription = "Cookbook Logo",
+                modifier = Modifier
+                    .size(250.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            // Title Text
+            Text(
+                text = "Sign up",
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold
+            )
 
-        //Spacer(modifier = Modifier.height(50.dp))
+            // Email Input
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = "Email:") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Password Input
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "Password:") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Confirm Password Input
+            OutlinedTextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                label = { Text(text = "Confirm Password:") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Domicilio Input
+            OutlinedTextField(
+                value = adress,
+                onValueChange = { adress = it },
+                label = { Text(text = "Address:") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            // Date Picker for Birthdate
+            OutlinedTextField(
+                value = birthDate,
+                onValueChange = {},
+                label = { Text(text = "Select Birthdate") },
+                modifier = Modifier.fillMaxWidth(),
+                readOnly = true,  // Para que el campo sea de solo lectura
+                trailingIcon = {
+                    IconButton(onClick = { datePickerDialog.show() }) {
+                        Icon(imageVector = Icons.Default.CalendarToday, contentDescription = null)
+                    }
+                }
+            )
+
+            // Gender Dropdown Menu
+            OutlinedTextField(
+                value = selectedGender,
+                onValueChange = {},
+                label = { Text(text = "Select Gender") },
+                modifier = Modifier.fillMaxWidth(),
+                readOnly = true,
+                trailingIcon = {
+                    IconButton(onClick = { expanded = true }) {
+                        Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = null)
+                    }
+                }
+            )
+
+            // Gender Dropdown Menu items
+            DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+                genderOptions.forEach { gender ->
+                    DropdownMenuItem(onClick = {
+                        selectedGender = gender
+                        expanded = false
+                    }) {
+                        Text(text = gender)
+                    }
+                }
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            //Boton de Sign Up
+            Button(
+                onClick = { navController.navigate(Routes.TitleView) },
+                colors = ButtonDefaults.buttonColors(Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .border(1.5.dp, Color(0xFFFFA500), RoundedCornerShape(25.dp))
+                    .shadow(10.dp, RoundedCornerShape(25.dp))
+            ) {
+                Text(text = "Sign Up", fontSize = 18.sp, color = Color(0xFFFFA500))
+            }
+        }
     }
+}
+//Falta implementar datos en el Drop menu
+fun DropdownMenuItem(onClick: () -> Unit, interactionSource: @Composable () -> Unit) {
+    TODO("Not yet implemented")
 }
 
 @Preview(showBackground = true)
