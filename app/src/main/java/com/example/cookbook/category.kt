2c757f1d07e75539a26c6613822e313281adcfd2
Mauricio Hernandez <mauriciohernandez@MacBook-Air-de-Mauricio.local>
2024-11-25 19:39:28 -0600

    cambios

--- app/src/main/java/com/example/cookbook/category.kt
+++ app/src/main/java/com/example/cookbook/category.kt
@@ -1,10 +1,5 @@
 package com.example.cookbook
 
-data class Category(
-    val _id: String,
-    val categoria: String?,
-    val category: String?
-) {
-    val name: String
-        get() = categoria ?: category ?: "Unknown"
-}
+data class category(
+    val id_category: String,
+)
